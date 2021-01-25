fd66032f73b9feeff27c629573f401e7e2995b0f
w0rng <antonabramov2000@gmail.com>
2021-01-25 21:03:21 +0700

    Новые переменные окружения и новый способ загрузки переменных

--- data/config.py
+++ data/config.py
@@ -1,9 +1,13 @@
-from environs import Env
+from os import environ
 
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
-env = Env()
-env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+TG_TOKEN = environ.get("TG_TOKEN", '')
+ADMINS = environ.get("ADMINS", '').split(',')
+DEBUG = environ.get('DEBUG', False).lower() == 'true'
+DATABASE = environ.get('DATABASE', 'sqlite:///:memory:')
+
+DOMAIN = environ.get('DOMAIN', None)
+WEBHOOK_PATH = f'/webhook/{TG_TOKEN}'
+WEBHOOK_URL = f'{DOMAIN}{WEBHOOK_PATH}'
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = environ.get('PORT', 0)
